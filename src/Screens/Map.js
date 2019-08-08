aab27209100662e0d5477025b540562030085113
apple <apple@apples-MacBook-Pro-3.local>
2019-08-08 19:17:37 +0530

    Added Search Autocomplete

--- src/Screens/Map.js
+++ src/Screens/Map.js
@@ -32,68 +32,72 @@ class Map extends Component {
     }
 
     render() {
-        return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'space-between'}}>
-                <GooglePlacesAutocomplete
-                    placeholder="Search"
-                    minLength={2} // minimum length of text to search
-                    autoFocus={false}
-                    returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
-                    listViewDisplayed="auto" // true/false/undefined
-                    fetchDetails={true}
-                    renderDescription={row => row.description} // custom description render
-                    onPress={(data, details = null) => {
-                        // 'details' is provided when fetchDetails = true
-                        console.log(data);
-                        console.log(details);
-                    }}
-                    getDefaultValue={() => {
-                        return ''; // text input default value
-                    }}
-                    query={{
-                        // available options: https://developers.google.com/places/web-service/autocomplete
-                        key: 'AIzaSyC2QhtACfVZ2cr9HVvxQuzxd3HT36NNK3Q',
-                        language: 'en', // language of the results
-                        types: '(cities)', // default: 'geocode'
-                    }}
-                    styles={{
-                        description: {
-                            fontWeight: 'bold',
-                        },
-                        predefinedPlacesDescription: {
-                            color: '#1faadb',
-                        },
-                    }}
-                    currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
-                    currentLocationLabel="Current location"
-                    nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-                    GoogleReverseGeocodingQuery={{
-                        // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-                    }}
-                    GooglePlacesSearchQuery={{
-                        // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-                        rankby: 'distance',
-                        types: 'food',
-                    }}
-                    filterReverseGeocodingByTypes={[
-                        'locality',
-                        'administrative_area_level_3',
-                    ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-                    predefinedPlaces={[homePlace, workPlace]}
-                    debounce={200}
-                />
+        const homePlace = {
+            description: 'Home',
+            geometry: { location: { lat: this.state.latitude, lng: this.state.longitude } },
+        };
+        const workPlace = {
+            description: 'Work',
+            geometry: { location: { lat: this.state.latitude, lng: this.state.longitude } },
+        };
 
+        return (
+            <View style={{flex: 1}}>
+                    <MapView.Callout style={{backgroundColor: "#fff", position: "absolute", top: 40, elevation: 10, width: "100%"}}>
+                        <GooglePlacesAutocomplete
+                            placeholder="Search"
+                            minLength={2} // minimum length of text to search
+                            autoFocus={false}
+                            returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+                            listViewDisplayed="auto" // true/false/undefined
+                            fetchDetails={true}
+                            renderDescription={row => row.description} // custom description render
+                            onPress={(data, details = null) => {
+                                // 'details' is provided when fetchDetails = true
+                                console.log(data);
+                                console.log(details);
+                            }}
+                            getDefaultValue={() => {
+                                return ''; // text input default value
+                            }}
+                            query={{
+                                // available options: https://developers.google.com/places/web-service/autocomplete
+                                key: 'AIzaSyC2QhtACfVZ2cr9HVvxQuzxd3HT36NNK3Q',
+                                language: 'en', // language of the results
+                                types: '(cities)', // default: 'geocode'
+                            }}
+                            styles={{
+                                description: {
+                                    fontWeight: 'bold',
+                                },
+                                predefinedPlacesDescription: {
+                                    color: '#1faadb',
+                                },
+                            }}
+                            currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+                            currentLocationLabel="Current location"
+                            nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+                            GoogleReverseGeocodingQuery={{
+                                // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+                            }}
+                            GooglePlacesSearchQuery={{
+                                // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+                                rankby: 'distance',
+                                types: 'food',
+                            }}
+                            filterReverseGeocodingByTypes={[
+                                'locality',
+                                'administrative_area_level_3',
+                            ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+                            predefinedPlaces={[homePlace, workPlace]}
+                            debounce={200}
+                        />
+                    </MapView.Callout>
                 <MapView
-                    style={{flex: 1, height: "100%", width: "100%"}}
+                    style={{flex: 1, height: "100%", width: "100%", elevation: -10}}
                     region={{ latitude: this.state.latitude, longitude: this.state.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421}}
                     showsUserLocation={true}
-
-                >
-                    <MapView.Marker
-                        coordinate={{latitude: this.state.latitude, longitude: this.state.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421}}
-                    />
-                </MapView>
-                <Text>Map</Text>
+                />
             </View>
         );
     }
